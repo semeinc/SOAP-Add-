b60f788298d29b599fa2fe402e26180b4dde23c2
MACOS <macos@macoss-imac-9.local>
2017-02-28 11:11:50 +0530

    Initial Commit

---
+++ SOAP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SOAP
+//
+//  Created by MACOS on 2/28/17.
+//  Copyright © 2017 king. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SOAP/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SOAP
+//
+//  Created by MACOS on 2/28/17.
+//  Copyright © 2017 king. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
